00c47db8ff7750b279d384783ff327ce4928f872
Seitosan <seitoworld@gmail.com>
2021-10-20 22:32:27 +0200

    add parse role

--- rbac-audit/src/main.go
+++ rbac-audit/src/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 func main() {
@@ -27,14 +30,52 @@ func main() {
 	//	RoleTable(roles, config, "roles")
 	//	RoleTable(clusterRoles, config, "clusterroles")
 	//}
+	data := [][]string{}
+	var description string
+	var result string
+	var configExclude []string
+	//var denyAPIGroups []string
+	var denyVerbs []string
+	//var denyResources []string
+	configExclude = config.File.Role.Exclude
+	//denyAPIGroups = config.File.Role.Deny.APIGroups
+	denyVerbs = config.File.Role.Deny.Verbs
+	//denyResources = config.File.Role.Deny.Ressources
 	fmt.Println(roles.Items)
 	for _, item := range roles.Items {
-		fmt.Println("#######")
-		if config.Verbosity.Level == "debug" {
-			log.Println("Parsing : " + item.Name + " Roles")
+		if InArray(item.Name, configExclude) {
+			log.Println(item.Name + "is excluded")
+		} else {
+			fmt.Println("#######")
+			if config.Verbosity.Level == "debug" {
+				log.Println("Parsing : " + item.Name + " Roles")
+			}
+			for _, rule := range item.Rules {
+				table := tablewriter.NewWriter(os.Stdout)
+				table.SetHeader([]string{"Object", "Details", "Result"})
+				for v := 0; v < len(rule.Verbs); v++ {
+					if InArray(rule.Verbs[v], denyVerbs) {
+						description = rule.Verbs[v]
+						result = "Deny"
+					} else {
+						description = rule.Verbs[v]
+						result = "Approved"
+					}
+					verbsResult := []string{"Verbs", description, result}
+					if result != "Approved" || config.Verbosity.Level == "debug" {
+						data = append(data, verbsResult)
+					}
+				}
+
+				for _, line := range data {
+					table.Append(line)
+				}
+				table.Render()
+			}
+			fmt.Println("#######")
+
 		}
-		fmt.Println(item.Rules)
-		fmt.Println("#######")
+
 	}
 	//fmt.Println(clusterRoles)
 	//fmt.Println(rolesBinding)
